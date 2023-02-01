58fe16b0f245528bbb18b29d93b7d5c0a9ebcb5b
Vishnu V R <mailvishnu98@gmail.com>
2023-02-01 18:07:34 +0530

    chat

--- src/Api/ChatRequest.js
+++ src/Api/ChatRequest.js
@@ -1,3 +1,3 @@
 import axios from 'axios'
-const API =axios.create({baseURL: "http://localhost:5000"})
+const API =axios.create({baseURL: process.env.REACT_APP_PHASE === "testing"? process.env.REACT_APP_DOMAIN_URL_TESTING : process.env.REACT_APP_DOMAIN_URL})
 export const userChats=(id)=>API.get(`/chat/${id}`)
